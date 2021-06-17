bfd0cad5e7d3835816558e50dd3656775e41b29d
Khambazarov <khambazarovrenat@gmail.com>
2021-06-17 20:50:50 +0200

    merge

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Datapod from renat
+          Datapod from renat!!!
         </p>
         <a
           className="App-link"
