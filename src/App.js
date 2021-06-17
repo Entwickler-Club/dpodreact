e3be10e6fd938902864925442295b1895eb3cd7c
NikoHamburg <nikotrettin@googlemail.com>
2021-06-17 20:49:41 +0200

    merge test

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Datapod from niko
+          Datapod from nikotest
         </p>
         <a
           className="App-link"
