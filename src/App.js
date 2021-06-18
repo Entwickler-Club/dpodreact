a6becddf4680d87950b4eb54a30a9672d13d913a
Stefan Ihle <stefanihle@gmx.net>
2021-06-18 09:29:13 +0200

    Delete some unneccessary questionmarks (#16)

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
 		<div className='App'>
 			<header className='App-header'>
 				<img src={logo} className='App-logo' alt='logo' />
-				<p>Datapod for React | What's up guys?????</p>
+				<p>Datapod for React | What's up guys?</p>
 				<a
 					className='App-link'
 					href='https://reactjs.org'
