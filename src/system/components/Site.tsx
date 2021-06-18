8de091b20810b5a90bf28905d97a5939e9f4d208
Edward Tanguay (hm.win) <edward@tanguay.info>
2021-06-18 18:32:48 +0200

    add router and menu

--- src/system/components/Site.tsx
+++ src/system/components/Site.tsx
@@ -1,3 +1,10 @@
+import React from "react";
+import {
+	BrowserRouter as Router,
+	Switch,
+	Route,
+	Link
+} from "react-router-dom";
 import '../styles/reset.scss';
 import '../styles/site.scss';
 import PageHome from '../../custom/components/PageHome';
@@ -7,10 +14,24 @@ function Site() {
 	return (
 		<div className="app_site">
 			<h1 className="siteTitle">Info Site</h1>
-			<section className="app_container">
-				<PageHome/>
-			</section>
-		</div>
+			<Router>
+				<nav>
+					<ul>
+						<li>
+							<Link to="/">Home</Link>
+						</li>
+					</ul>
+				</nav>
+
+				<section className="app_container">
+					<Switch>
+						<Route path="/">
+							<PageHome />
+						</Route>
+					</Switch>
+				</section>
+			</Router>
+		</div >
 	)
 }
 
