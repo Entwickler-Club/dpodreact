b9384f530d61101ef86280fc827411a8be31c6a1
Edward Tanguay (hm.win) <edward@tanguay.info>
2021-06-25 22:12:20 +0200

    reset

--- src/system/components/Site.tsx
+++ src/system/components/Site.tsx
@@ -7,8 +7,8 @@ import PageHome from '../../custom/components/PageHome';
 
 // DYNAMIC_CODE_AREA: loadPageComponentLines
 import PageShowcaseCounterUsingState from './PageShowcaseCounterUsingState'; // ::showcaseCounterUsingState
-import PageShowcaseTest from './PageShowcaseTest'; // ::showcaseTest 
-import PageShowcaseLodash from './PageShowcaseLodash'; // ::showcaseLodash 
+import PageShowcaseTest from './PageShowcaseTest'; // ::showcaseTest
+import PageShowcaseLodash from './PageShowcaseLodash'; // ::showcaseLodash
 
 function Site() {
 	const { isShowing, setIsShowing } = useContext(ShowContext);
