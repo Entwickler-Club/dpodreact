c7a5c500b95fe37e0fd1fd491b078351aa86654f
edwardstudent <79741992+edwardstudent@users.noreply.github.com>
2021-06-28 19:20:53 +0200

    Create page Showcase TypeScript for Class Interfaces (#25)
    * finish except for title
    
    * finished
    
    Co-authored-by: Edward Tanguay (hm.win) <edward@tanguay.info>

---
+++ src/system/components/PageShowcaseTypeScriptClasses.tsx
@@ -0,0 +1,49 @@
+import { useState } from 'react';
+import '../styles/showcaseTypeScriptClasses.scss';
+
+interface IMessageManager {
+	title: string;
+	addMessage(arg: string): void;
+	getMessage(): string;
+}
+
+class MessageManager implements IMessageManager {
+
+	public title: string;
+	private messages: string[];
+
+	constructor() {
+		this.messages = [];
+		this.title = 'Message Manager';
+	}
+
+	addMessage(message: string) {
+		this.messages.push(message);
+	}
+
+	getMessage(): string {
+		return `the ${this.title} class says ${this.messages.pop()}` || '';
+	}
+}
+
+function PageShowcaseTypeScriptClasses() {
+
+	const [message, setMessage] = useState('___________');
+
+	const messageManager = new MessageManager();
+	messageManager.addMessage('testing');
+
+	const changeValue = () => {
+		setMessage(messageManager.getMessage());
+	}
+	return (
+		<div className="page page_showcaseTypeScriptClasses">
+			<h2 className="title">Showcase: TypeScript Classes</h2>
+			<p className="description">An info page that displays how to use TypeScript classes with interfaces.</p>
+			<p>The message from the message manager is <span className="message">{message}</span>.</p>
+			<button onClick={() => changeValue()}>Change value</button>
+		</div>
+	)
+}
+
+export default PageShowcaseTypeScriptClasses;
--- src/system/components/Site.tsx
+++ src/system/components/Site.tsx
@@ -8,6 +8,7 @@ import PageHome from '../../custom/components/PageHome';
 // DYNAMIC_CODE_AREA: loadPageComponentLines
 import PageShowcaseCounterUsingState from './PageShowcaseCounterUsingState'; // ::showcaseCounterUsingState
 import PageShowcaseLodash from './PageShowcaseLodash'; // ::showcaseLodash
+import PageShowcaseTypeScriptClasses from './PageShowcaseTypeScriptClasses';// ::showcaseTypeScriptClasses
 
 function Site() {
 	const { isShowing, setIsShowing } = useContext(ShowContext);
@@ -42,6 +43,7 @@ function Site() {
 								{/* DYNAMIC_JSX_AREA: linkPageComponentLines */}
 								<li><Link to='/showcaseCounterUsingState'>Showcase: Counter Using State</Link></li> {/* ::showcaseCounterUsingState */}
 								<li><Link to='/showcaseLodash'>Showcase: Lodash</Link></li> {/* ::showcaseLodash */}
+								<li><Link to='/showcaseTypeScriptClasses'>Showcase: TypeScript Classes</Link></li>{/* ::showcaseTypeScriptClasses */}
 							</>
 						)}
 					</ul>
@@ -55,6 +57,7 @@ function Site() {
 						{/* DYNAMIC_JSX_AREA: routePageComponentLines */}
 						<Route path='/showcaseCounterUsingState'><PageShowcaseCounterUsingState /></Route> {/* ::showcaseCounterUsingState */}
 						<Route path='/showcaseLodash'><PageShowcaseLodash /></Route> {/* ::showcaseLodash */}
+						<Route path='/showcaseTypeScriptClasses'><PageShowcaseTypeScriptClasses /></Route>{/* ::showcaseTypeScriptClasses */}
 					</Switch>
 				</section>
 			</div>
