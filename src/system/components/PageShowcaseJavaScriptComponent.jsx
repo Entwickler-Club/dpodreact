cdc859b20373de4d13652c6c55c845119ad7c5b1
Edward Student <edwardstudent@tanguay.info>
2021-06-28 19:37:01 +0200

    small change javascript component

--- src/system/components/PageShowcaseJavaScriptComponent.jsx
+++ src/system/components/PageShowcaseJavaScriptComponent.jsx
@@ -7,7 +7,7 @@ function PageShowcaseJavaScriptComponent() {
 		<div className="page page_showcaseJavaScriptComponent">
 			<h2 className="title">Showcase: JavaScript Component</h2>
 			<p className="description">This is a test of a JavaScript component.</p>	
-			<p>To create a JavaScript components, create a TypeScript page first with <code>npm run dpod -- createPage --pageTitle "Test Page"</code> and then rename the component to .jsx.</p>
+			<p>To create a JavaScript component, create a TypeScript page first with <code>npm run dpod -- createPage --pageTitle "Test Page"</code> and then rename the component to .jsx, and then do a <code>npm start</code> again.</p>
 		</div>
 	)
 }
