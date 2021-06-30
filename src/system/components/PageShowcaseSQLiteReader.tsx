ba1bbc069c38179c6d4b82ba25c6e899c2fc1bc2
Edward Student <edwardstudent@tanguay.info>
2021-06-30 20:18:28 +0200

    create SQLite Reader showcase page

---
+++ src/system/components/PageShowcaseSQLiteReader.tsx
@@ -0,0 +1,14 @@
+import '../styles/showcaseSQLiteReader.scss';
+
+function PageShowcaseSQLiteReader() {
+
+	return (
+		<div className="page page_showcaseSQLiteReader">
+			<h2 className="title">Showcase: SQLite Reader</h2>
+			<p className="description">An info page that displays showcase: SQLite reader.</p>	
+			<p className="message">Welcome to this page.</p>
+		</div>
+	)
+}
+
+export default PageShowcaseSQLiteReader;
