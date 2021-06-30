90714c6e98aac0008e7dd6ebd2745f129ad807cf
Edward Student <edwardstudent@tanguay.info>
2021-06-30 20:31:47 +0200

    add test data

--- src/system/components/PageShowcaseSQLiteReader.tsx
+++ src/system/components/PageShowcaseSQLiteReader.tsx
@@ -1,12 +1,23 @@
+import { useState } from 'react';
 import '../styles/showcaseSQLiteReader.scss';
 
 function PageShowcaseSQLiteReader() {
 
+	const [records] = useState([{ id: 1, message: "test message" }]);
+
 	return (
 		<div className="page page_showcaseSQLiteReader">
 			<h2 className="title">Showcase: SQLite Reader</h2>
-			<p className="description">An info page that displays showcase: SQLite reader.</p>	
-			<p className="message">Welcome to this page.</p>
+			<p className="description">An info page that displays showcase: SQLite reader.</p>
+			<div className="recordArea">
+				<ul>
+					{records.map((record: any) => (
+						<>
+							<li>{record.id} - {record.message}</li>
+						</>
+					))}
+				</ul>
+			</div>
 		</div>
 	)
 }
