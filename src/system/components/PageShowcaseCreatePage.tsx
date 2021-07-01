b4cb0fb716109c52ce95c0cf97400d2c066e1a8a
Edward Student <edwardstudent@tanguay.info>
2021-07-01 20:17:04 +0200

    make form and fetch

--- src/system/components/PageShowcaseCreatePage.tsx
+++ src/system/components/PageShowcaseCreatePage.tsx
@@ -1,12 +1,33 @@
+import { useState, useEffect } from 'react';
 import '../styles/showcaseCreatePage.scss';
 
 function PageShowcaseCreatePage() {
+	const [pageTitle, setPageTitle] = useState('');
+	useEffect(() => {
+	}, []);
+	const createPage = () => {
+		setPageTitle('changed');
+		fetch('http://localhost:5001/sqliteTest')
+			.then(response => response.json())
+			.then((data: any) => {
+				console.log(data);
+			});
+	}
+
+	const onPageTitleChange = (event: any) => {
+		setPageTitle(event.target.value);
+	}
 
 	return (
 		<div className="page page_showcaseCreatePage">
 			<h2 className="title">Showcase: Create Page</h2>
-			<p className="description">An info page that displays showcase: create page.</p>	
-			<p className="message">Welcome to this page.</p>
+			<p className="description">An info page that displays showcase: create page.</p>
+			<form>
+				<label htmlFor="pageTitle">Page Title</label>
+				<input type="text" value={pageTitle} onChange={onPageTitleChange} id="pageTitle" />
+				<button type="button" onClick={() => createPage()}>Submit</button>
+			</form>
+			[{pageTitle}]
 		</div>
 	)
 }
