a518e1cdb546ee81b59f7600ed16b0593f69d389
Edward Student <edwardstudent@tanguay.info>
2021-08-17 19:59:17 +0200

    action arrives in controller

--- src/system/controllers/controller.ts
+++ src/system/controllers/controller.ts
@@ -2,16 +2,21 @@ class Controller {
 
     protected request: any;
     protected response: any;
-    // protected actionMethodName: string;
+    protected actionMethodName: string;
+    protected action: string;
+    protected requestData: any;
 
     constructor(request: any, response: any) {
         this.request = request;
         this.response = response;
-        //this.actionMethodName = 'action_' + this.action;
+        this.requestData = this.request.body;
+        this.action = this.requestData.action;
+        this.actionMethodName = 'action_' + this.action;
     }
 
     process() {
-        console.log('controller is processing222');
+        // this[this.actionMethodName]();
+        console.log(this.actionMethodName);
         // app.post('/controllerShowcaseJsonReadWrite', (req: any, res: any) => {
         // 	const {action} = req.body;
         // 	fs.readFile('./src/system/data/json/itemType_pageItems.json', 'utf-8', (err: any, rawData: any) => {
