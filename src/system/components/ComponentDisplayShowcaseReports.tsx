d94d881b85e706f5d3560c6b145b8d35f7684721
Edward Student <edwardstudent@tanguay.info>
2021-09-28 21:30:06 +0200

    items are showing

--- src/system/components/ComponentDisplayShowcaseReports.tsx
+++ src/system/components/ComponentDisplayShowcaseReports.tsx
@@ -1,37 +1,25 @@
-import { IShowcaseReport } from '../dataLayer/interfaces';
 import ShowcaseReports from '../itemTypes/showcaseReports';
 import ShowcaseReport from '../itemTypes/showcaseReport';
+import ComponentDisplayShowcaseReport from './ComponenDisplayShowcaseReport';
 
 interface IComponentDisplayShowcaseReportsProps {
     items: ShowcaseReports;
 }
 
-export const ComponentDisplayShowcaseReport = (props: IComponentDisplayShowcaseReportsProps) => {
+export const ComponentDisplayShowcaseReports = (props: IComponentDisplayShowcaseReportsProps) => {
     const showcaseReports = props.items;
     const showcaseReportsItemsArray = showcaseReports.getItems<ShowcaseReport>();
     const item: ShowcaseReport = new ShowcaseReport(); //showcaseReportsItemsArray[0];
     const tests = showcaseReportsItemsArray.map(m => m.get_title());
     return (
         <>
-            <div>test:: {showcaseReportsItemsArray.length}</div>
             {showcaseReportsItemsArray.map((showcaseReport: ShowcaseReport) => {
                 return (
-                    <div>{showcaseReport.get_title()}</div>
+                    <ComponentDisplayShowcaseReport item={showcaseReport} />
                 )
             })}
-            {/* <div>{item.get_title()}</div> */}
-            {/* {showcaseReportsItemsArray.map((showcaseReport: ShowcaseReport) => {
-                return (
-                    <div>{showcaseReport.get</div>
-                )
-            })} */}
-            {/* {showcaseReportsItemsArray.map((showcaseReport: ShowcaseReport, index: number) => {
-                return (
-                    <div>nnn</div>
-                )
-            })} */}
         </>
     )
 }
 
-export default ComponentDisplayShowcaseReport;
+export default ComponentDisplayShowcaseReports;
