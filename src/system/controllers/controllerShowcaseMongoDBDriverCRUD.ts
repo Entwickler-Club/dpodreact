556223806d575653607f45b8071b75987c09ca73
Edward Tanguay (hm.win) <edward@tanguay.info>
2021-11-12 21:13:37 +0100

    action with mongo works

--- src/system/controllers/controllerShowcaseMongoDBDriverCRUD.ts
+++ src/system/controllers/controllerShowcaseMongoDBDriverCRUD.ts
@@ -4,19 +4,10 @@ import { MongoClient } from 'mongodb';
 
 const client = new MongoClient('mongodb://localhost:27017');
 
-const main = async () => {
+const getData = async (callback: any) => {
 	await client.connect();
 	const db = client.db('northwind');
-
-	const products = await db.collection('products').find({ ProductName: /en/i })
-		.project({
-			ProductID: 1,
-			ProductName: 1,
-			_id: 0
-		}).toArray();
-	console.log(products);
-
-	return 'done';
+	callback(db);
 }
 
 class ControllerShowcaseMongoDBDriverCRUD extends Controller {
@@ -26,15 +17,37 @@ class ControllerShowcaseMongoDBDriverCRUD extends Controller {
 	}
 
 	action_loadPageData() {
-		main()
-			.then(console.log)
-			.catch(console.error)
-			.finally(() => client.close());
-		this.response.send({
-			message: 'Welcome to this page.'
+		getData(async (db: any) => {
+			const products = await db.collection('products').find({ ProductName: /en/i })
+				.project({
+					ProductID: 1,
+					SupplierID: 1,
+					ProductName: 1,
+					_id: 0
+				}).toArray();
+			this.response.send({
+				message: products.length
+			});
 		});
 	}
 
+	// action_loadPageData() {
+	// 	const getData = async ((db:any) => {
+	// 	const products = await db.collection('products').find({ ProductName: /en/i })
+	// 		.project({
+	// 			ProductID: 1,
+	// 			ProductName: 1,
+	// 			_id: 0
+	// 		}).toArray();
+	// 	console.log(products);
+	// this.response.send({
+	// 	message: 'Welcome to this page.'
+	// });
+	// });
+	// getData()
+	// 	.then(console.log)
+	// 	.catch(console.error)
+	// 	.finally(() => client.close());
 }
 
 export default ControllerShowcaseMongoDBDriverCRUD;
